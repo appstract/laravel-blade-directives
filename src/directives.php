7bb9953aa7a67f9794767b29af38e6db9375a197
Olav van Schie <olavschie@hotmail.com>
2017-06-13 17:46:53 +0200

    .

--- src/directives.php
+++ src/directives.php
@@ -35,7 +35,7 @@ return [
     'mix' => function ($expression) {
         if (ends_with($expression, ".css'")) {
             return '<link rel="stylesheet" href="<?php echo mix('.$expression.') ?>">';
-        } else if (ends_with($expression, ".js'")) {
+        } elseif (ends_with($expression, ".js'")) {
             return '<script src="<?php echo mix('.$expression.') ?>"></script>';
         }
 
