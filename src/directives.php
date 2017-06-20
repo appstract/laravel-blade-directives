bf3c6bf532b4da766330bc8082e33d46c944804a
Olav van Schie <olavschie@hotmail.com>
2017-06-20 15:13:38 +0200

    is false

--- src/directives.php
+++ src/directives.php
@@ -26,6 +26,22 @@ return [
         return '<?php endif; ?>';
     },
 
+    'isfalse' => function ($expression) {
+        if (str_contains($expression, ',')) {
+            $expression = DirectivesRepository::parseMultipleArgs($expression);
+
+            return  "<?php if (isset({$expression->get(0)}) && (bool) {$expression->get(0)} === false) : ?>".
+                "<?php echo {$expression->get(1)}; ?>".
+                '<?php endif; ?>';
+        }
+
+        return "<?php if (isset({$expression}) && (bool) {$expression} === false) : ?>";
+    },
+
+    'endisfalse' => function ($expression) {
+        return '<?php endif; ?>';
+    },
+
     /*
     |---------------------------------------------------------------------
     | @mix
