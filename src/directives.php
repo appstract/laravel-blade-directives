62d3701d04c9f555ca8be12a6988e41d7497ccac
Federico Liva <fede91it@gmail.com>
2017-06-27 15:34:48 +0200

    Fix StyleCI

--- src/directives.php
+++ src/directives.php
@@ -186,13 +186,13 @@ return [
 
     'pushonce' => function ($expression) {
         list($push_name, $push_sub) = explode(':', trim(substr($expression, 1, -1)));
-        $k = '__pushonce_' . $push_name . '_' . $push_sub;
+        $k = '__pushonce_'.$push_name.'_'.$push_sub;
 
         return "<?php if(!isset(\$__env->{$k})): \$__env->{$k} = 1; \$__env->startPush('{$push_name}'); ?>";
     },
 
     'endpushonce' => function () {
         return '<?php $__env->stopPush(); endif; ?>';
-    }
+    },
 
 ];
