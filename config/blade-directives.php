c743fcaa1f5b2f817eadc2575c889b588a46bd4f
Miguel Piedrafita <github@miguelpiedrafita.com>
2017-10-20 20:42:03 +0200

    Support Blade conditionals

--- config/blade-directives.php
+++ config/blade-directives.php
@@ -7,12 +7,18 @@ return [
     | Directives
     |--------------------------------------------------------------------------
     |
-    | This is a list of directives you'd like to register.
+    | This is a list of directives and conditionals you'd like to register.
     | Use the directive name as key, and a closure.
     |
     */
 
     'directives' => [
+
+        // 'name' => function($expresion) {}
+
+    ],
+
+    'conditionals' => [
 
         // 'name' => function($expresion) {}
 
