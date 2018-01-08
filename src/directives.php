398d36a1c1f2740c81358b99473fd1564a81c406
Olav van Schie <olavschie@hotmail.com>
2018-01-08 14:58:34 +0100

    Support kebab-case in pushonce

--- src/directives.php
+++ src/directives.php
@@ -234,7 +234,7 @@ return [
     'pushonce' => function ($expression) {
         list($pushName, $pushSub) = explode(':', trim(substr($expression, 1, -1)));
 
-        $key = '__pushonce_'.$pushName.'_'.$pushSub;
+        $key = '__pushonce_'.$pushName.'_'.str_replace('-', '_', $pushSub);
 
         return "<?php if(! isset(\$__env->{$key})): \$__env->{$key} = 1; \$__env->startPush('{$pushName}'); ?>";
     },
